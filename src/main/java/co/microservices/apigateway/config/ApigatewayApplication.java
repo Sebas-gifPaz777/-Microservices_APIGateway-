6a70b9146b314dce4a2d4dfe4a532a95c20aa243
Duvan R. Cuero <47835668+merolemay@users.noreply.github.com>
2025-04-21 17:40:01 -0500

    configuration to load apis

--- src/main/java/co/microservices/apigateway/config/ApigatewayApplication.java
+++ src/main/java/co/microservices/apigateway/config/ApigatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"co.microservices.apigateway", "co.microservices"})
+@ComponentScan(basePackages = {"co.microservices.apigateway"})
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
